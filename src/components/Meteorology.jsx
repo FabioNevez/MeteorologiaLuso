8c70db704caf710ab8ed8381324ffbfeee6117cd
Fábio <fabionevez@outlook.pt>
2021-03-23 20:22:18 +0000

    change link

--- src/components/Meteorology.jsx
+++ src/components/Meteorology.jsx
@@ -205,7 +205,7 @@ function Meteorology(props) {
 
     useEffect(() => {
         
-        fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${k}?apikey=${process.env.REACT_APP_SECRET_KEY}&language=en-US&details=false&metric=true`)
+        fetch(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${k}?apikey=${process.env.REACT_APP_SECRET_KEY}&language=en-US&details=false&metric=true`)
         .then(res => res.json())
         .then(
             (result) => {
