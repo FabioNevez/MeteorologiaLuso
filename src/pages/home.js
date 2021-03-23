62fd910305412c747683ca623388898c63906dfe
Fábio <fabionevez@outlook.pt>
2021-03-23 18:48:41 +0000

    change text

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,7 +7,7 @@ function Home() {
     return (
         <div className="home">
         <h1>Olá!</h1>
-        <h2>Escolha um país da Lusofonia que deseja saber a previsão meteorológica!</h2>
+        <h2>Para começar escolha um país da Lusofonia que deseja saber a previsão meteorológica!</h2>
         <img src={worldImage} alt="World"></img>
         </div>
     )
